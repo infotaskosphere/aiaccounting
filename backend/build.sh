9e5f4fbcf6c93f66d40eb3dd7e43cf353462df5e
infotaskosphere <info.taskosphere@gmail.com>
2026-04-19 19:41:40 +0530

    Update build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -1,19 +1,3 @@
 #!/usr/bin/env bash
-# backend/build.sh
 set -e
-
-# System dependencies
-apt-get update -qq
-apt-get install -y -qq \
-  tesseract-ocr \
-  tesseract-ocr-eng \
-  poppler-utils \
-  libpango-1.0-0 \
-  libpangocairo-1.0-0 \
-  libcairo2 \
-  libgdk-pixbuf2.0-0 \
-  libffi-dev \
-  shared-mime-info
-
-# Python dependencies
-pip install -r requirements.txt
+pip install -r requirements-render.txt
